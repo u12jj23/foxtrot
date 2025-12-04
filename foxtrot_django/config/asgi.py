54d29a159eacc64e1cf4d872766b2b217f9c0b30
Jep <u12jj23@abdn.ac.uk>
2025-12-04 00:51:48 +0000

    Update settings, asgi, manage and runtime config

--- foxtrot_django/config/asgi.py
+++ foxtrot_django/config/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foxtrot_django.config.settings')
+
 
 application = get_asgi_application()
